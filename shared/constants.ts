0f725098f8067649ce1c8311a0fedeffc65080b5
Hessamoddin A Shokravi <ahs502@gmail.com>
2023-05-16 18:32:43 +0330

    Upgrade official dependencies

--- shared/constants.ts
+++ shared/constants.ts
@@ -1,5 +1,4 @@
-export const ZETTEL_EXTENSION_ACCESS_KEY = process.env.ZETTEL_EXTENSION_ACCESS_KEY || ''
-
 export const ZETTEL_TARGET_ENVIRONMENT = (process.env.ZETTEL_TARGET_ENVIRONMENT || '') as 'local' | 'stage' | 'live'
+export const ZETTEL_EXTENSION_ACCESS_KEY = process.env.ZETTEL_EXTENSION_ACCESS_KEY || ''
 
 export const SERVER_BASE_URL = process.env.SERVER_BASE_URL || ''
